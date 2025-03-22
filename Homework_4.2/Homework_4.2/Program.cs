dde0488be5db3152d367241320b397d2bb3f41c2
MehmetAliAkcali <mehmetali.akcali@std.yeditepe.edu.tr>
2025-03-22 14:36:00 +0300

    Add files via upload

---
+++ Homework_4.2/Homework_4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Matematik matematik = new Matematik();
+
+        // Tamsayı toplama
+        Console.WriteLine("2 + 3 = " + matematik.Topla(2, 3));
+        Console.WriteLine("2 + 3 + 4 = " + matematik.Topla(2, 3, 4));
+
+        // Ondalıklı toplama
+        Console.WriteLine("2.5 + 3.5 = " + matematik.Topla(2.5, 3.5));
+        Console.WriteLine("2.5 + 3.5 + 4.5 = " + matematik.Topla(2.5, 3.5, 4.5));
+
+        // Tamsayı çarpma
+        Console.WriteLine("2 * 3 = " + matematik.Carp(2, 3));
+        Console.WriteLine("2 * 3 * 4 = " + matematik.Carp(2, 3, 4));
+
+        // Ondalıklı çarpma
+        Console.WriteLine("2.5 * 3.5 = " + matematik.Carp(2.5, 3.5));
+        Console.WriteLine("2.5 * 3.5 * 4.5 = " + matematik.Carp(2.5, 3.5, 4.5));
+    }
+}
---
+++ Homework_4.2/Homework_4.2/matematik.cs
@@ -0,0 +1,50 @@
+public class Matematik
+{
+    
+    public int Topla(int sayi1, int sayi2)
+    {
+        return sayi1 + sayi2;
+    }
+
+    
+    public int Topla(int sayi1, int sayi2, int sayi3)
+    {
+        return sayi1 + sayi2 + sayi3;
+    }
+
+    
+    public double Topla(double sayi1, double sayi2)
+    {
+        return sayi1 + sayi2;
+    }
+
+    
+    public double Topla(double sayi1, double sayi2, double sayi3)
+    {
+        return sayi1 + sayi2 + sayi3;
+    }
+
+    
+    public int Carp(int sayi1, int sayi2)
+    {
+        return sayi1 * sayi2;
+    }
+
+    
+    public int Carp(int sayi1, int sayi2, int sayi3)
+    {
+        return sayi1 * sayi2 * sayi3;
+    }
+
+    
+    public double Carp(double sayi1, double sayi2)
+    {
+        return sayi1 * sayi2;
+    }
+
+    
+    public double Carp(double sayi1, double sayi2, double sayi3)
+    {
+        return sayi1 * sayi2 * sayi3;
+    }
+}
